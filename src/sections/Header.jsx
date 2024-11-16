b86a1515719fc8e5f5389a71e2fb762050c14ae8
Josh Zarazovsi <joshuazarazovsi@gmail.com>
2024-11-16 13:12:56 +1100

    Added Logo for mobile

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -13,9 +13,9 @@ const Header = () => {
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <img src="/images/xora.svg" width={155} height={55} alt="xora" />
         </a>
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left:0 max-lg:w-full max-lg:s-2 max-lg: opacity-0">
+        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left:0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
           <div className="w-full max-lg:relaive max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
-            <nav className="max-lg:relative max-lg:z-2 max-lg:px-12">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
                   <NavLink title="features" />
@@ -24,14 +24,17 @@ const Header = () => {
                 </li>
 
                 <li className="nav-logo">
-                  <LinkScroll>
+                  <LinkScroll className="max-lg:hidden transition-transform duration-500 cursor-pointer">
                     <img
                       src="/images/xora.svg"
                       width={160}
                       height={55}
-                      alt="xora"
+                      alt="logo"
                     />
                   </LinkScroll>
+                </li>
+
+                <li className="nav-li">
                   <NavLink title="faq" />
                   <div className="dot" />
                   <NavLink title="download" />
@@ -40,6 +43,9 @@ const Header = () => {
             </nav>
           </div>
         </div>
+        <button className="lg:hidden float-end z-2 size-10 border-2 border-s4/s25 rounded-full flex justify-center items-center">
+          <img src="./images/magic.svg" width={25} height={25} alt="magic" />
+        </button>
       </div>
     </header>
   );
