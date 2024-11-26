06bd26dc12d498f6dc8cb78c1d51dcf3f335bad1
Josh Zarazovsi <joshuazarazovsi@gmail.com>
2024-11-26 22:05:59 +1100

    Completely finished Features | Began Pricing section

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -7,11 +7,12 @@ const Features = () => {
     <section>
      <Element name="features">
 			<div className="container">
-				<div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7 md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
+				<div className="relative flex md:flex-wrap flex-nowrap md:overflow-hidden max-md:flex-col feature-after md:g7 border-2
+				 border-s3 rounded-[44px] max-md:border-2 max-md:rounded-none">
 				{features.map(({ id, icon, caption, title, text, button }) => (
               <div
                 key={id}
-                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:flex-320"
+                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:flex-320"
               >
                 <div className="w-full flex justify-start items-start">
                   <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
